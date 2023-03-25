ff167ae85690e32dfcbb9de6f1c831fe232b531d
unknown <ernest.mampana@gmail.com>
2023-03-26 01:10:06 +0200

    google places search update

--- lib/backend/keys.dart
+++ lib/backend/keys.dart
@@ -1,9 +1,3 @@
 class Keys {
-  static const key = 'AIzaSyAYcgECVHjmtlNXTIXQ0eVJTJIY3wkaNB4';
-  static const String googleMapsAndroidKey =
-      "AIzaSyDW5ueY7LF-y4-gsJ3dHUOaOvRKKrRz8sE";
-  static const String googleMapsIosKey =
-      "AIzaSyCFB3ctksuSbvth-HyO9AwfnLAboCq3R1A";
 
-      //AIzaSyB2TWHONvLIUNBz29hs82Ff7ZBWYa3Zy3Y
 }
